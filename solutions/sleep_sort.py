26d21a526f804262fd81911aad0af9b91ebbf37c
David Williamson <davidw135@gmail.com>
2019-07-01 18:32:45 -0700

    sleep sort WIP
    Signed-off-by: David Williamson <davidw135@gmail.com>

---
+++ solutions/sleep_sort.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+"""Sleep Sort
+
+In general, sleep sort works by starting a separate task for each item to be
+sorted, where each task sleeps for an interval corresponding to the item's sort
+key, then emits the item. Items are then collected sequentially in time.
+
+Task: Write a program that implements sleep sort. Have it accept non-negative
+integers on the command line and print the integers in sorted order. If this
+is not idomatic in your language or environment, input and output may be done
+differently.
+
+https://rosettacode.org/wiki/Sorting_algorithms/Sleep_sort
+
+"""
+
+
+def main():
+    """main"""
+    raise NotImplementedError("Currently under development")
+
+
+if __name__ == '__main__':
+    main()
